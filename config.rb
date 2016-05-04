5373a666bd760704a6e4859079afd94398a49b44
naitadar <naitadar@gmail.com>
2016-05-04 22:44:39 +0300

    sass: adding sass mixin

--- config.rb
+++ config.rb
@@ -2,11 +2,13 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
+http_path = "/app"
+css_dir = "css"
+sass_dir = "styles"
 images_dir = "images"
-javascripts_dir = "javascripts"
+
+output_style = :compact
+line_comments = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
